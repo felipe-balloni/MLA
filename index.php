7609cdc5874a09abe493aa85e002f67a77fbe666
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2023-03-18 17:02:31 -0300

    construct property promotion

--- index.php
+++ index.php
@@ -1,2 +1,42 @@
 <?php
-echo "Hello World!";
+
+class Service
+{
+    public function execute(): string
+    {
+        return "This class is runnung";
+    }
+}
+
+//class Controller
+//{
+//    private $service;
+//
+//    public function __construct(Service $service)
+//    {
+//        $this->service = $service;
+//    }
+//
+//    public function handle()
+//    {
+//        echo $this->service->execute();
+//    }
+//}
+
+class Controller
+{
+
+    public function __construct(
+        private Service $service
+    ) {
+    }
+
+    public function handle(): void
+    {
+        echo $this->service->execute();
+    }
+}
+
+$controller = new Controller(new Service());
+
+echo $controller->handle();
