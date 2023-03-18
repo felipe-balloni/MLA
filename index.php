69877ef62d9ea304bddba2f5d27e583495e99e7c
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2023-03-18 17:39:07 -0300

    Enums with functions

--- index.php
+++ index.php
@@ -5,13 +5,22 @@ enum Status: int
     case PAID = 1;
     case PENDING = 2;
     case CANCELED = 0;
+
+    public function color(): string
+    {
+        return match ($this) {
+            self::PAID => 'green',
+            self::PENDING => 'yellow',
+            self::CANCELED => 'red',
+        };
+    }
 }
 
 class CheckoutService
 {
-    public function handle(Status $gatewayStatus): int
+    public function handle(Status $gatewayStatus)
     {
-        return $gatewayStatus->value;
+        return $gatewayStatus->color();
     }
 }
 
